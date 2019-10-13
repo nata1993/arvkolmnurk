05c8276419d6c52efcd10c8b16eb2345bbe1149d
kebo <bogdan.parubok@hotmail.com>
2019-10-13 19:56:35 +0300

    unfinished number triangle

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Arvukolmnurk
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            for (int i = 1; i <= 5 ; i++)
+            {
+                Console.WriteLine($"{i}");
+
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write($"{j}");   
+
+                }
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
