c6be3cc1b31ef24dfec2a55e1173d307c08cccee
kebo <bogdan.parubok@hotmail.com>
2019-10-26 16:29:18 +0300

    final version of arvukolmnurk, fixed the bug

--- Program.cs
+++ Program.cs
@@ -10,15 +10,16 @@ namespace Arvukolmnurk
             for (int i = 1; i <= 5 ; i++)
             {
                 Console.WriteLine($"{i}");
-
+                
                 for (int j = 1; j <= i; j++)
                 {
-                    Console.Write($"{j}");   
-
-                }
-                
+                    if (i == 5)
+                    {
+                        break;
+                    }
+                    Console.Write($"{j}");                 
+                }               
             }
-            Console.ReadLine();
         }
     }
 }
