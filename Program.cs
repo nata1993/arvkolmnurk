404e43291a02321e1284d3530ce9b6d6e666a879
kebo <bogdan.parubok@hotmail.com>
2019-10-26 16:37:09 +0300

    added comment of how it works

--- Program.cs
+++ Program.cs
@@ -13,12 +13,14 @@ namespace Arvukolmnurk
                 
                 for (int j = 1; j <= i; j++)
                 {
-                    if (i == 5)
-                    {
-                        break;
-                    }
+                    if (i == 5) //without break, the trianle ends with two 12345 lines at the end. This happens because at the end, i = 5 but j gets assigned as 1. 
+                    {           //Since 1 is smaller than 5, which is stated in second for as j <= i, the second for loop continues until j reaches 5 at with point first loop
+                        break;  //ends because i is incremented to 6 and since first loop is stated to end at 5 (i <= 5), then loop breaks and leaves us with two lines of 12345.
+                    }           //Break breaks second loop by checking if the i is 5 and since after seconf for loop is nothing written, first loop is started again, i gets incremented to 6
+                                // and then i <= 5 breaks first loop, since 6 is bigger than 5.
                     Console.Write($"{j}");                 
-                }               
+                }    
+                //here is nothing witten.
             }
         }
     }
